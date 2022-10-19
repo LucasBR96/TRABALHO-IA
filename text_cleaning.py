217025ca37261e639ab409ac6a1b4e98480c0a3c
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2022-10-19 18:57:12 -0300

    ja tenho uma nocao

--- text_cleaning.py
+++ text_cleaning.py
@@ -1,17 +1,33 @@
 import re
+import unicodedata as und
 
 EXCESS_WHITE = r"( {2,}|\t+|\n+)"
-PONCTUATION  = r"[\?!,\.]"
+PONCTUATION  = r"[\?!,\.\":]"
+LINK         = r"(https?:?//\S+|@\w+)"
 
-def normalize_str( entry_str : str ):
+HASHTAG      = r"(?<!\S)#\S+"
+
+def normalize_str( entry_str : str ) -> str:
 
     text = entry_str.lower()
+
+    #----------------------------------------------------------------
+    # removing diacritics
+    text = und.normalize( 'NFD' , text )
+    shaved = ''.join( c for c in text if not und.combining( c ) )
+    text   = und.normalize( 'NFC' , shaved )
 
     #-----------------------------------------------------------------
     #removing unecessary characters
     text = re.sub( EXCESS_WHITE , " " , text )  # excess whitespace
-    text = re.sub( PONCTUATION , "" , text ) 
+    text = re.sub( PONCTUATION , "" , text )    # ponctuation 
     
-    print( text )
+    return text
 
-normalize_str( "@_Ioen imagino...  ow mandei fazer um body opressor pra Duda! hahahaha" )
+def remove_special_words( entry_str : str ) -> str:
+    return re.sub( LINK , "" , entry_str )
+
+norm = normalize_str( "A afronta aos portugueses não pára! https://t.co/mGEV4CjFRP#RenovarPortugal #PNR #Portugal #Imigração https://t.co/26j98jdIYH" )
+print( norm )
+
+print( remove_special_words( norm ) )
