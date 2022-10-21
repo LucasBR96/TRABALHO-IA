524cd3dde4170a2ec560305233edb3561bd6c0e7
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2022-10-21 10:27:28 -0300

    pre merge

--- text_cleaning.py
+++ text_cleaning.py
@@ -4,10 +4,11 @@ import pandas as pd
 import nltk
 
 EXCESS_WHITE = r"( {2,}|\t+|\n+)"
-PONCTUATION  = r"[\?!,\.\":\(\)\“\”]"
+PONCTUATION  = r"[\?!,\.\":\(\)\“\”\-]"
 LINK         = r"(https?:?//\S+|@\w+)"
-
 HASHTAG      = r"(?<!\S)#\S+"
+
+STOP_WORDS = set( nltk.corpus.stopwords.words('portuguese') )
 
 def normalize_str( entry_str : str ) -> str:
 
@@ -29,7 +30,16 @@ def normalize_str( entry_str : str ) -> str:
 def remove_special_words( entry_str : str ) -> str:
     return re.sub( LINK , "" , entry_str )
 
-norm = normalize_str( "A Alemanha tornou a sentir os “benefícios” dos (i)”migrantes” da invasão islâmica." )
-print( norm )
+def remove_stopwords( entry_str : str ) -> str:
+    words : list[ str ] = entry_str.split( sep = " " )
+    return " ".join( word for word in words if not( word in STOP_WORDS ) )
 
-print( remove_special_words( norm ) )
+def clear_text( entry_str : str ) -> str:
+
+    norm : str = normalize_str( entry_str )
+    norm = remove_special_words( norm )
+    return remove_stopwords( norm )
+
+
+norm = clear_text( "A coisa q mais me da odio na vida é mulher machista. Qnd escuto alguma falando merda fico com mt abuso - pq odeio gnt burra e ignorante tb" )
+print( norm )
