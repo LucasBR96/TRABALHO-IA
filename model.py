9d5d20b94f4e8786cbed6117232cc313ce991fff
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2022-10-25 14:46:27 -0300

    rascunhando modelo

--- model.py
+++ model.py
@@ -6,7 +6,6 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 import numpy as np
 
-
 class minha_net( nn.Module ):
 
     def __init__( self ):
@@ -30,6 +29,21 @@ class minha_net( nn.Module ):
     def foward( self , x ):
         return self.seq( x )
 
+
+def learning_step( X , y , mod : minha_net, opm : torch.optim.SGD , loss_fn ):
+    
+    y_prime = mod( X )
+    train_loss = loss_fn( y , y_prime )
+    opm.zero_grad( )
+    train_loss.backward()
+    opm.step()
+
+    return train_loss
+
+def val_step( X_val , y_val , mod , loss_fn ):
+    
+    y_hat = mod( X_val )
+    return loss_fn( y_val , y_hat ) 
 
 m = minha_net()
 for name, param in m.named_parameters():
