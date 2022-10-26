bc651040ccd2ea30cefa1497223ebdd5d5136ef3
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2022-10-26 15:06:19 -0300

    comecando o modelo

--- model.py
+++ model.py
@@ -3,8 +3,10 @@ import sys
 
 import torch
 from torch import nn
+from torch.utils.data import DataLoader
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+PATH = 'DATA\modelo.pt'
 
 import numpy as np
 
@@ -65,16 +67,15 @@ def bin_F1_score( y_hat , y ):
     precision = TP/( TP + FP )
     recall    = TP/( TP + FN )
 
-    return 2*precision*recall/( precision + recall )
+    return 100*( 1 - 2*precision*recall/( precision + recall ) )
 
-#-------------------------------------------------------------------------------------------
-# Full batch, not used in "production"
-def learning_loop( X , y , X_val , y_val , lr = 1e-3 , batch_iters = 40 , num_batchs = 100  ):
+def learning_loop( train_data , eval_data , lr = 1e-3 , batch_iters = 40 , num_batchs = 100  ):
 
     opm = torch.optim.SGD( lr = lr )
-    def loss( y , y_hat ):
-        return -100*bin_F1_score( y_hat , y )
+    
     mod = minha_net()
+    train_set = DataLoader( train_data , batch_size = 50 , shuffle = True )
+    test_set =  DataLoader( eval_data , batch_size = 50 , shuffle = True )
 
     iters = []
     t_costs = []
@@ -82,10 +83,14 @@ def learning_loop( X , y , X_val , y_val , lr = 1e-3 , batch_iters = 40 , num_ba
     last_v_loss = sys.maxsize
     for i in range( num_batchs ):
         for j in range( batch_iters ):
-            t_loss = learning_step( X , y , mod , opm , loss )
-        v_loss = val_step( X_val , y_val , mod , loss )
+            X , y = next( train_set )
+            t_loss = learning_step( X , y , mod , opm , bin_F1_score )
+        X_val , y_val = next( train_set )
+        v_loss = val_step( X_val , y_val , mod , bin_F1_score )
 
-        if v_loss < last_v_loss
+        if v_loss < last_v_loss:
+            last_v_loss = v_loss
+            torch.save(mod.state_dict(), PATH)
 
         iters.append( ( i + 1 )*batch_iters )
         t_costs.append( t_loss )
